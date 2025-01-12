0d13d07d9bb45de796fad6060825a7092e434e91
Antonia <antoniapopeanga37@gmail.com>
2025-01-12 22:02:01 +0200

    Testing and small corrections

--- graphql/queries/reportsWrittenByAUserQuery.js
+++ graphql/queries/reportsWrittenByAUserQuery.js
@@ -2,7 +2,7 @@ import { GraphQLList, GraphQLInt } from 'graphql';
 import db from '../../models/index.js';
 import reportType from '../types/reportType.js';
 
-const reportsOfAUserQueryResolver = async (_ , args) => {
+const reportsWrittenByAUserQueryResolver = async (_ , args) => {
     const { userId } = args
     const user = await db.User.findOne({
         where: {
@@ -12,21 +12,21 @@ const reportsOfAUserQueryResolver = async (_ , args) => {
     if(!user){
         return null;
     }else{
-        const reportsOfAUser = await db.Report.findAll({
+        const reportsWrittenByAUser = await db.Report.findAll({
             where: {
                 userId
             },
         });
-        return reportsOfAUser;
+        return reportsWrittenByAUser;
     }
 }
 
-const reportsOfAUserQuery = {
+const reportsWrittenByAUserQuery = {
     type: new GraphQLList(reportType),
     args: {
         userId: { type: GraphQLInt },
     },
-    resolve: reportsOfAUserQueryResolver,
+    resolve: reportsWrittenByAUserQueryResolver,
 };
 
-export default reportsOfAUserQuery;
+export default reportsWrittenByAUserQuery;
